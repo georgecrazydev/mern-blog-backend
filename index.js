5ad7707334513bdb1eb35dac159d5113f90051e4
georgecrazydev <g.paichadze1996@gmail.com>
2023-03-15 00:02:38 +0400

    create user model, express valitaion for register, register user jwt token

--- index.js
+++ index.js
@@ -1,40 +1,62 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { validationResult } from 'express-validator';
+
+import { registerValidator } from './validations/auth.js';
+import UserModel from './models/User.js';
 
 dotenv.config();
-
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log('DB ok!'))
+  .then(() => console.log('DB connected!'))
   .catch((err) => console.log('DB Err', err));
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Giorgi sss');
-});
+app.post('/auth/register', registerValidator, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-app.post('/auth/login', (req, res) => {
-  console.log(req.body);
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-  const token = jwt.sign(
-    {
-      name: 'giorgi',
+    const doc = new UserModel({
       email: req.body.email,
-    },
-    'secret123',
-  );
-  res.json({
-    success: true,
-    token,
-  });
+      fullName: req.body.fullName,
+      passwordHash: hash,
+      avatarUrl: req.body.avatarUrl,
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret123',
+      { expiresIn: '30d' },
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: `Failed to register` });
+  }
 });
+
 app.listen(8000, (err) => {
   if (err) {
     return console.log(err);
   }
-  console.log('Server ok!');
+  console.log('server running!');
 });
